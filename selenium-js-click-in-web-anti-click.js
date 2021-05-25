d940e29db38b614ff0c8f1cb0b75604ce892d001
Trung Nguyễn <48853223+nttrung9x@users.noreply.github.com>
2021-05-25 23:42:11 +0700

    Create selenium-js-click-in-web-anti-click.js

---
+++ selenium-js-click-in-web-anti-click.js
@@ -0,0 +1,31 @@
+# Code này dùng để set value và click chuột vào element khi dùng selenium mà bị web chặn
+
+function setNativeValue(element, value) {
+  let lastValue = element.value;
+  element.value = value;
+  let event = new Event('input', { target: element, bubbles: true });
+  event.simulated = true;
+  let tracker = element._valueTracker;
+  if (tracker) {
+    tracker.setValue(lastValue);
+  }
+  element.dispatchEvent(event);
+}
+let input = document.getElementById('{element}');
+setNativeValue(input, '{value}');
+
+
+const mouseClickEvents = ['mousedown', 'click', 'mouseup'];
+function simulateMouseClick(element){
+  mouseClickEvents.forEach(mouseEventType =>
+    element.dispatchEvent(
+      new MouseEvent(mouseEventType, {
+         view: window,
+         bubbles: true,
+         cancelable: true,
+         buttons: 1
+  })));
+}
+
+let element =document.document.getElementById('username')
+simulateMouseClick(element);
