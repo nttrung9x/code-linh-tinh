492def3a7e0830947cee47e4c689e67e4b2e6833
Trung Nguyễn <48853223+nttrung9x@users.noreply.github.com>
2022-03-12 10:14:50 +0700

    Create selenium-v4-run-profile-using-proxy-authen-ip-port-user-pass.cs

---
+++ selenium-v4-run-profile-using-proxy-authen-ip-port-user-pass.cs
@@ -0,0 +1,26 @@
+// Document: https://www.selenium.dev/documentation/webdriver/bidirectional/bidi_api/
+
+// Code share By www.VipSoft247.Com
+public void RunProfileBy_VipSoft247_Com()
+{
+    var options = new ChromeOptions {AcceptInsecureCertificates = true};
+    options.AddArgument("headless");
+    options.Proxy = new Proxy {HttpProxy = "1.1.1.1:12345", SslProxy = "1.1.1.1:12345", Kind = ProxyKind.Manual};
+    //options.AddArguments($"--proxy-server=http://1.1.1.1:12345");
+    var driver = new ChromeDriver(ChromeDriverService.CreateDefaultService(), options, TimeSpan.FromMinutes(2));
+
+    NetworkAuthenticationHandler handler = new NetworkAuthenticationHandler()
+    {
+        UriMatcher = d => true, //d.Host.Contains("your-host.com")
+        Credentials = new PasswordCredentials("proxy_user", "proxy_pass")
+    };
+
+    var networkInterceptor = driver.Manage().Network;
+    networkInterceptor.AddAuthenticationHandler(handler);
+    networkInterceptor.StartMonitoring();
+
+    _driver.Navigate().GoToUrl("https://stackoverflow.com/");
+
+    networkInterceptor.StopMonitoring();
+    driver.Quit();
+}
