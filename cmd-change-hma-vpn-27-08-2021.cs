3ba32f5a02bd3fb65946349e1301a955327ca805
Trung Nguyễn <48853223+nttrung9x@users.noreply.github.com>
2022-09-12 23:54:13 +0700

    Update cmd-change-hma-vpn-27-08-2021.cs

--- cmd-change-hma-vpn-27-08-2021.cs
+++ cmd-change-hma-vpn-27-08-2021.cs
@@ -1,14 +1,20 @@
-            public void change_hma()
-            {
-                        Process process = new Process();
-                        ProcessStartInfo startInfo = new ProcessStartInfo();
-                        startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                        startInfo.FileName = "cmd.exe";
-                        startInfo.Arguments = "/C netsh interface set interface \"HMA! Pro VPN\" disable";
-                        startInfo.Verb = "runas";
-                        process.StartInfo = startInfo;
-                        process.Start();
-            }
+public void change_hma()
+{
+/*
+https://www.facebook.com/hkvn9x
+skype: nttrung9x
+https://mmo69.com
+telegram: hkvn9x
+*/
+      Process process = new Process();
+      ProcessStartInfo startInfo = new ProcessStartInfo();
+      startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+      startInfo.FileName = "cmd.exe";
+      startInfo.Arguments = "/C netsh interface set interface \"HMA! Pro VPN\" disable";
+      startInfo.Verb = "runas";
+      process.StartInfo = startInfo;
+      process.Start();
+}
 
 
 
