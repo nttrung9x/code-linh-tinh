6a7a207cc5381c6ed15a4d759dbc44421bdcd41d
Trung Nguyễn <48853223+nttrung9x@users.noreply.github.com>
2024-09-21 19:01:48 +0700

    Create ReCaptcha-demo.py

---
+++ ReCaptcha-demo.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+# Khởi tạo driver với extension từ thư mục
+options = webdriver.ChromeOptions()
+options.add_argument("--load-extension=D:\\path_to\\all.recaptcha.v6.9.captcha69.com")  # Đường dẫn đến thư mục extension
+options.add_argument("--proxy-bypass-list=captcha69.com");// bỏ qua proxy khi load trang web captcha69.com - áp dụng cho cả requests bên trong extensions
+
+driver = webdriver.Chrome(options=options)
+
+# Mở trang Facebook
+driver.get('https://www.facebook.com')
+
+# Loop để kiểm tra captcha có được giải thành công hay chưa
+Giai_Thanh_Cong_Text = "Giải Xong - CaptCha69.Com"
+while True:
+    elements = driver.find_elements(By.CLASS_NAME, "ReCaptcha_solver")
+    for element in elements:
+        if Giai_Thanh_Cong_Text in element.text:
+            print("Captcha giải thành công!")
+            break
+    else:
+        sleep(1)
+        continue
+    break
+
+# Đóng driver sau khi hoàn thành
+driver.quit()
